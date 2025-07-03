e28c6d2d9561ed824a7b86a2f9a9a5f5b4ba58e9
Satoshi Konno <skonno@cybergarage.org>
2025-07-03 13:15:23 +0900

    Update Job interface

--- job/job.go
+++ job/job.go
@@ -14,16 +14,89 @@
 
 package job
 
+import (
+	"fmt"
+)
+
 // Job represents a job that can be scheduled to run at a specific time or interval.
 type Job interface {
 	// Kind returns the name of the job.
 	Kind() string
 	// Handler returns the job handler for the job.
 	Handler() JobHandler
-	// Payload returns the payload of the job.
-	Payload() any
 	// Process processes the job using the job handler.
 	Process() error
 	// String returns a string representation of the job.
 	String() string
+}
+
+type job struct {
+	name    string
+	logger  Logger
+	handler *jobHandler
+	ctx     *jobContext
+}
+
+// JobOption is a function that configures a job.
+type JobOption func(*job)
+
+// WithJobName sets the name of the job.
+func WithJobName(name string) JobOption {
+	return func(j *job) {
+		j.name = name
+	}
+}
+
+// WithJobStartedAt sets the start time of the job.
+func WithJobLogger(logger Logger) JobOption {
+	return func(j *job) {
+		j.logger = logger
+	}
+}
+
+// NewJob creates a new job with the given name and options.
+func NewJob(opts ...any) (Job, error) {
+	j := &job{
+		name:    "",
+		handler: newJobHandler(),
+		ctx:     newJobContext(),
+		logger:  NewNullLogger(),
+	}
+
+	for _, opt := range opts {
+		switch opt := opt.(type) {
+		case JobOption:
+			opt(j)
+		case JobHandlerOption:
+			opt(j.handler)
+		case JobContextOption:
+			opt(j.ctx)
+		default:
+			return nil, fmt.Errorf("invalid job option type: %T", opt)
+		}
+	}
+
+	return j, nil
+}
+
+// Kind returns the name of the job.
+func (j *job) Kind() string {
+	return j.name
+}
+
+// Handler returns the handler of the job.
+func (j *job) Handler() JobHandler {
+	return j.handler
+}
+
+func (j *job) Process() error {
+	// Implement the logic to process the job
+	// For example, you might want to execute the job's command or function
+	// and update its state in the database or in-memory store.
+	return nil
+}
+
+// String returns a string representation of the job.
+func (j *job) String() string {
+	return j.name
 }
