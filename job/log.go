fcd86a9a622724ab892199b550304919e9281795
Satoshi Konno <skonno@cybergarage.org>
2025-07-11 13:11:43 +0900

    Update Log implementation

--- job/log.go
+++ job/log.go
@@ -25,3 +25,80 @@ type Log interface {
 	Level() LogLevel
 	Message() string
 }
+
+type log struct {
+	ts    time.Time
+	job   Job
+	level LogLevel
+	msg   string
+}
+
+// NewLog creates a new log entry.
+type LogOption func(*log)
+
+// WithLogJob sets the job associated with the log entry.
+func WithLogJob(job Job) LogOption {
+	return func(l *log) {
+		l.job = job
+	}
+}
+
+// WithLogLevel sets the log level of the log entry.
+func WithLogLevel(level LogLevel) LogOption {
+	return func(l *log) {
+		l.level = level
+	}
+}
+
+// WithLogMessage sets the message of the log entry.
+func WithLogMessage(msg string) LogOption {
+	return func(l *log) {
+		l.msg = msg
+	}
+}
+
+// WithLogTimestamp sets the timestamp of the log entry.
+func WithLogTimestamp(ts time.Time) LogOption {
+	return func(l *log) {
+		l.ts = ts
+	}
+}
+
+// NewLog creates a new log entry with the specified options.
+func NewLog(opts ...LogOption) Log {
+	l := &log{
+		ts:    time.Now(),
+		job:   nil,
+		level: LogInfo,
+		msg:   "",
+	}
+	for _, opt := range opts {
+		opt(l)
+	}
+	return l
+}
+
+// Job returns the job associated with the log entry.
+func (l *log) Job() Job {
+	return l.job
+}
+
+// Timestamp returns the timestamp of the log entry.
+func (l *log) Timestamp() time.Time {
+	return l.ts
+}
+
+// Level returns the log level of the log entry.
+func (l *log) Level() LogLevel {
+	return l.level
+}
+
+// Message returns the message of the log entry.
+func (l *log) Message() string {
+	return l.msg
+}
+
+// String returns the string representation of the log entry.
+func (l *log) String() string {
+	return l.ts.Format(time.RFC3339) + " [" + l.level.String() + "] " + l.msg
+}
