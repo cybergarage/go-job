f70487cbbe2410f4b911134c804fc0984b8a4479
Satoshi Konno <skonno@cybergarage.org>
2025-07-14 19:14:44 +0900

    Add example tests

---
+++ job/example_test.go
@@ -0,0 +1,48 @@
+// Copyright (C) 2025 The go-job Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package job
+
+import (
+	"fmt"
+	"time"
+)
+
+func Example() {
+	// Create a job manager
+	mgr := NewManager()
+
+	// Register a job with a custom executor
+	sumJob, _ := NewJob(
+		WithKind("sum"),
+		WithExecutor(func(a, b int) int { return a + b }),
+		WithScheduleAt(time.Now()),
+		WithResponseHandler(func(inst Instance, res []any) {
+			fmt.Printf("Result: %v\n", res)
+		}),
+	)
+	mgr.RegisterJob(sumJob)
+
+	// Schedule the registered job
+	mgr.ScheduleRegisteredJob("sum", WithArguments(1, 2))
+
+	// Start the job manager
+	mgr.Start()
+
+	// Wait for the job to complete
+	mgr.StopWithWait()
+
+	// Output:
+	// Result: [3]
+}
