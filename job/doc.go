29384464745657f42178d1cc3383f4cce042c80c
Satoshi Konno <skonno@cybergarage.org>
2025-07-17 09:19:00 +0900

    Update overview comments for godoc

--- job/doc.go
+++ job/doc.go
@@ -1,5 +1,20 @@
-// Package job provides utilities for managing and executing jobs
-// within the go-job project.
-// It includes job scheduling, execution, and queue management.
+// Copyright (C) 2025 The go-job Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
+/*
+go-job is a flexible and extensible job scheduling and execution library for Go. It enables you to register, schedule, and manage jobs with arbitrary function signatures, supporting custom executors, priorities, and advanced scheduling options such as cron expressions and delayed execution.
+
+The library provides robust job observation features, including state and log history tracking, as well as customizable response and error handlers. With support for distributed storage backends, `go-job` is suitable for both local and distributed environments, making it ideal for building scalable, reliable job processing systems in Go applications.
+*/
 package job
