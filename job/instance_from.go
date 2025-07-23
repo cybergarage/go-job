80fda9c39d117c690aacb20e63ce29bc61f080f1
Satoshi Konno <skonno@cybergarage.org>
2025-07-23 19:29:53 +0900

    Add NewInstancesFromQueue()

---
+++ job/instance_from.go
@@ -0,0 +1,59 @@
+// Copyright (C) 2025 The go-job Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package job
+
+import (
+	"github.com/google/uuid"
+)
+
+// NewInstancesFromQueue creates a list of job instances from the provided queue.
+func NewInstancesFromQueue(queue Queue) ([]Instance, error) {
+	list, err := queue.List()
+	if err != nil {
+		return nil, err
+	}
+	return list, nil
+}
+
+// NewInstancesFromStore creates a list of job instances from the provided store.
+func NewInstancesFromHistory(history InstanceHistory) ([]Instance, error) {
+	jiOptsMap := make(map[uuid.UUID][]any)
+	for _, state := range history {
+		jiOpts, ok := jiOptsMap[state.UUID()]
+		if !ok {
+			jiOpts = make([]any, 0)
+			jiOpts = append(jiOpts, WithUUID(state.UUID()))
+			jiOpts = append(jiOpts, WithKind(state.Kind()))
+		}
+		switch state.State() {
+		case JobCreated:
+			jiOpts = append(jiOpts, WithCreatedAt(state.Timestamp()))
+		case JobCompleted:
+			jiOpts = append(jiOpts, WithCompletedAt(state.Timestamp()))
+		case JobTerminated:
+			jiOpts = append(jiOpts, WithTerminatedAt(state.Timestamp()))
+		}
+		jiOptsMap[state.UUID()] = jiOpts
+	}
+	jiList := make([]Instance, 0, len(jiOptsMap))
+	for _, instanceOpts := range jiOptsMap {
+		ji, err := NewInstance(instanceOpts...)
+		if err != nil {
+			return nil, err
+		}
+		jiList = append(jiList, ji)
+	}
+	return jiList, nil
+}
