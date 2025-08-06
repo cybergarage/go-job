5572e1acdf5df68e05c57ba761dd0a1492581b9f
Satoshi Konno <skonno@cybergarage.org>
2025-08-06 12:06:38 +0900

    fead: add Filter interface for Store interface

---
+++ job/filter.go
@@ -0,0 +1,84 @@
+// Copyright (C) 2025 The go-job Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package job
+
+import (
+	"time"
+)
+
+// Filter is an interface that defines methods for filtering job instances.
+type Filter interface {
+	// Before returns the time before which job instances should be filtered.
+	Before() (time.Time, bool)
+	// IsUnset returns true if no filter criteria are configured.
+	IsUnset() bool
+	// Matches checks if the specified object matches the filter criteria.
+	Matches(v any) bool
+}
+
+// FilterOption is a function that configures a job filter.
+type FilterOption func(*filter)
+
+type filter struct {
+	before time.Time
+}
+
+// WithFilterBefore sets the time before which job instances should be filtered.
+func WithFilterBefore(before time.Time) FilterOption {
+	return func(f *filter) {
+		f.before = before
+	}
+}
+
+// NewFilter creates a new instance of Filter with the given options.
+func NewFilter(opts ...FilterOption) Filter {
+	f := &filter{
+		before: time.Time{},
+	}
+	for _, opt := range opts {
+		opt(f)
+	}
+	return f
+}
+
+// Before returns the time before which job instances should be filtered.
+func (f *filter) Before() (time.Time, bool) {
+	if f.before.IsZero() {
+		return time.Time{}, false
+	}
+	return f.before, true
+}
+
+// IsUnset returns true if no filter criteria are configured.
+func (f *filter) IsUnset() bool {
+	_, ok := f.Before()
+	return !ok
+}
+
+// Matches checks if the specified object matches the filter criteria.
+func (f *filter) Matches(v any) bool {
+	switch v := v.(type) {
+	case Instance:
+		if before, ok := f.Before(); ok && !v.CreatedAt().Before(before) {
+			return true
+		}
+		return false
+	case instanceState:
+		if before, ok := f.Before(); ok && !v.Timestamp().Before(before) {
+			return true
+		}
+	}
+	return false
+}
---
+++ jobtest/filter_test.go
@@ -0,0 +1,62 @@
+// Copyright (C) 2025 The go-job Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package jobtest
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/cybergarage/go-job/job"
+)
+
+func TestFilter(t *testing.T) {
+	t.Helper()
+
+	tests := []struct {
+		opts           []job.FilterOption
+		expectedBefore bool
+	}{
+		{
+			opts:           []job.FilterOption{},
+			expectedBefore: false,
+		},
+		{
+			opts: []job.FilterOption{
+				job.WithFilterBefore(time.Time{}),
+			},
+			expectedBefore: false,
+		},
+		{
+			opts: []job.FilterOption{
+				job.WithFilterBefore(time.Now()),
+			},
+			expectedBefore: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("before(%t)", tt.expectedBefore), func(t *testing.T) {
+			query := job.NewFilter(tt.opts...)
+			before, ok := query.Before()
+			if ok != tt.expectedBefore {
+				t.Errorf("expected Before() to return %t, got %t", tt.expectedBefore, ok)
+			}
+			if tt.expectedBefore && before.IsZero() {
+				t.Errorf("expected Before() to return a non-zero time, got zero time")
+			}
+		})
+	}
+}
