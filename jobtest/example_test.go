4a08b1bb3a25139e0b1e05ffe28bc5398dcb4618
Satoshi Konno <skonno@cybergarage.org>
2025-08-20 15:10:18 +0900

    test: update Example()

--- jobtest/example_test.go
+++ jobtest/example_test.go
@@ -28,7 +28,10 @@ func Example() {
 	// Register a job with a custom executor
 	sumJob, _ := job.NewJob(
 		job.WithKind("sum"),
-		job.WithExecutor(func(a, b int) int { return a + b }),
+		job.WithExecutor(func(ji job.Instance, a, b int) int {
+			ji.Debugf("sum(%d, %d)", a, b) // Log the input values using job.Instance method
+			return a + b
+		}),
 		job.WithStateChangeProcessor(func(ji job.Instance, state job.JobState) {
 			ji.Infof("State changed to: %v", state)
 		}),
@@ -44,7 +47,7 @@ func Example() {
 
 	// Schedule the registered job
 	ji, _ := mgr.ScheduleRegisteredJob("sum",
-		job.WithArguments(1, 2),
+		job.WithArguments("?", 1, 2),   // "?" is a dummy placeholder for job.Instance
 		job.WithScheduleAt(time.Now()), // immediate scheduling is the default, so this option is redundant
 	)
 
