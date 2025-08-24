b0241366ce3cb9fb199add2edf1204ed46e424f5
Satoshi Konno <skonno@cybergarage.org>
2025-08-24 22:26:01 +0900

    docs: add documents for job history and log cleaners

---
+++ job/plugins/job/doc.go
@@ -0,0 +1,4 @@
+// Package job provides pluggable system job backends for go-job.
+//
+// This package offers useful system jobs such as job history and log cleanup.
+package job
---
+++ job/plugins/job/example_test.go
@@ -0,0 +1,53 @@
+// Copyright (C) 2025 The go-job Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package job
+
+import (
+	"time"
+
+	"github.com/cybergarage/go-job/job"
+)
+
+func ExampleNewHistoryCleaner() {
+	// Create a job manager
+	mgr, _ := job.NewManager()
+
+	// Register the history cleaner job
+	mgr.RegisterJob(
+		NewHistoryCleaner(),
+	)
+	// Schedule the job with the manager
+	mgr.ScheduleRegisteredJob(
+		HistoryCleaner,
+		job.WithCrontabSpec("0 0 * * *"), // Every day at midnight
+		job.WithArguments("?", "?", time.Now().Add(-30*24*time.Hour)), // Delete history older than 30 days
+	)
+}
+
+func ExampleNewLogCleaner() {
+	// Create a job manager
+	mgr, _ := job.NewManager()
+
+	// Register the log cleaner job
+	mgr.RegisterJob(
+		NewLogCleaner(),
+	)
+	// Schedule the job with the manager
+	mgr.ScheduleRegisteredJob(
+		LogCleaner,
+		job.WithCrontabSpec("0 0 * * *"), // Every day at midnight
+		job.WithArguments("?", "?", time.Now().Add(-30*24*time.Hour)), // Delete log older than 30 days
+	)
+}
