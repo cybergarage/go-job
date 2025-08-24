916661e3d1d64c10a88652abcda7b5575e6d22b2
Satoshi Konno <skonno@cybergarage.org>
2025-08-24 21:45:28 +0900

    fead(plugin): add job log cleaner plugin

---
+++ job/plugins/job/log_cleaner.go
@@ -0,0 +1,46 @@
+// Copyright (C) 2025 The go-job Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package job
+
+import (
+	"time"
+
+	"github.com/cybergarage/go-job/job"
+)
+
+const (
+	LogCleaner = "system.log.cleaner"
+)
+
+// NewLogCleaner returns a job that cleans up old job instance logs.
+// The job executor accepts the following parameters:
+//   - mgr: job.Manager - The job manager to perform the cleanup operation.
+//   - ji: job.Instance - The job instance representing the log cleaner job.
+//   - before: time.Time - A timestamp indicating that all job instances completed before this time should be deleted.
+func NewLogCleaner() job.Job {
+	cleaner, _ := job.NewJob(
+		job.WithKind(LogCleaner),
+		job.WithExecutor(func(mgr job.Manager, ji job.Instance, before time.Time) {
+			filter := job.NewFilter(
+				job.WithFilterBefore(before),
+			)
+			err := mgr.ClearInstanceLogs(filter)
+			if err != nil {
+				ji.Errorf("Failed to clear job logs: %v", err)
+			}
+		}),
+	)
+	return cleaner
+}
